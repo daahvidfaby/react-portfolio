6b1f5e6bd123b76ea98d8380ee5a38a1bd683bbd
David Faby <contact@davidfaby.fr>
2017-03-22 08:10:32 +0100

    Revision oh HTML structure for a better parallax effect

--- src/App.js
+++ src/App.js
@@ -107,76 +107,78 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="container">
-        <header className="header">
-          <div className="logo">
-            <h2 className="logo__text">
-              David <span className="logo__text--green">Faby</span>
-            </h2>
+        <div className="parallax">
+          <div className="parallax__layer parallax__layer--front">
+            <header className="header">
+              <div className="logo">
+                <h2 className="logo__text">
+                  David <span className="logo__text--green">Faby</span>
+                </h2>
+              </div>
+              <nav className="menu">
+                <div className="menu__button">
+                  <a href="#" className="menu__button__link">
+                    <img className="menu__button__icon" src={menu__icon} alt="Menu" />
+                    <div className="menu__button__text">Menu</div>
+                  </a>
+                </div>
+                <ul className="menu__list">
+                  <li className="menu__list__item">Présentation</li>
+                  <li className="menu__list__item">Projets</li>
+                  <li className="menu__list__item">Contact</li>
+                </ul>
+              </nav>
+            </header>
+            <section className="intro">
+              <div className="intro__text">
+                <p className="intro__text__paragraph  intro__text__paragraph--primary">Bonjour !</p>
+                <p className="intro__text__paragraph"><span className="intro__text__paragraph--light">Je suis</span> David Faby,</p>
+                <h1 className="intro__title intro__title--close intro__title--code">Développeur<br/> Front-end</h1>
+              </div>
+              <div className="intro__pattern"></div>
+            </section>
           </div>
-          <nav className="menu">
-            <div className="menu__button">
-              <a href="#" className="menu__button__link">
-                <img className="menu__button__icon" src={menu__icon} alt="Menu" />
-                <div className="menu__button__text">Menu</div>
-              </a>
-            </div>
-            <ul className="menu__list">
-              <li className="menu__list__item">Présentation</li>
-              <li className="menu__list__item">Projets</li>
-              <li className="menu__list__item">Contact</li>
-            </ul>
-          </nav>
-        </header>
-        <main className="content parallax">
-          <section className="intro parallax__layer parallax__layer--front">
-            <div className="intro__text">
-              <p className="intro__text__paragraph  intro__text__paragraph--primary">Bonjour !</p>
-              <p className="intro__text__paragraph"><span className="intro__text__paragraph--light">Je suis</span> David Faby,</p>
-              <h1 className="intro__title intro__title--close intro__title--code">Développeur<br/> Front-end</h1>
-            </div>
-            <div className="intro__pattern">
+          <main className="content parallax__layer parallax__layer--back">
+            <section className="content-block content-block--first">
+              <img src={david} alt="David Faby" className="content-block__image image--parallax"/>
+              <article className="article">
+                <h2 className="article__title">Présentation</h2>
+                <p className="article__text">
+                Apprenti en <Strong>développement web</Strong>, j’aime
+                réaliser des sites et des applications web
+                interactives et modernes.
+                Plutôt orienté <Strong>Front-end</Strong>,
+                je suis <Strong>polyvalent</Strong> lors de leur création.
+                Pour vous, je saurais à la fois me pencher
+                sur la conception, pour réfléchir au
+                développement à toutes les étapes d’un
+                projet.
+                Que ce soit sur la <Strong>réflexion</Strong> sur l’<Strong>expérience utilisateur</Strong>,
+                l’<Strong>interface</Strong> et l’<Strong>accessibilité</Strong>, ou
+                lors du <Strong>développement back-end</Strong> et
+                front-end, je saurais utiliser mes
+                compétences pour mener à bien ce projet.
+                Je sais m’intégrer à une équipe et à la fois travailler en autonomie.
+                Ensemble, nous pourrons créer l’avenir de l’internet.
+                </p>
+              </article>
+            </section>
+            <section className="content-block content-block--dark">
+              <article className="article">
+                <h2 className="article__title">Compétences</h2>
+                  <SkillsBlock title="Intégration" skills="integration"/>
+                  <SkillsBlock title="Développement web" skills="development"/>
+                  <SkillsBlock title="Automatisation" skills="automating"/>
+                  <SkillsBlock title="Interfaces" skills="interfaces"/>
+              </article>
 
+            </section>
+            <div className="home-contact">
+              <Button location="/contact">Contactez-moi</Button>
             </div>
-          </section>
-          <section className="content-block content-block--first">
-            <img src={david} alt="David Faby" className="content-block__image image--parallax"/>
-            <article className="article">
-              <h2 className="article__title">Présentation</h2>
-              <p className="article__text">
-              Apprenti en <Strong>développement web</Strong>, j’aime
-              réaliser des sites et des applications web
-              interactives et modernes.
-              Plutôt orienté <Strong>Front-end</Strong>,
-              je suis <Strong>polyvalent</Strong> lors de leur création.
-              Pour vous, je saurais à la fois me pencher
-              sur la conception, pour réfléchir au
-              développement à toutes les étapes d’un
-              projet.
-              Que ce soit sur la <Strong>réflexion</Strong> sur l’<Strong>expérience utilisateur</Strong>,
-              l’<Strong>interface</Strong> et l’<Strong>accessibilité</Strong>, ou
-              lors du <Strong>développement back-end</Strong> et
-              front-end, je saurais utiliser mes
-              compétences pour mener à bien ce projet.
-              Je sais m’intégrer à une équipe et à la fois travailler en autonomie.
-              Ensemble, nous pourrons créer l’avenir de l’internet.
-              </p>
-            </article>
-          </section>
-          <section className="content-block content-block--dark">
-            <article className="article">
-              <h2 className="article__title">Compétences</h2>
-                <SkillsBlock title="Intégration" skills="integration"/>
-                <SkillsBlock title="Développement web" skills="development"/>
-                <SkillsBlock title="Automatisation" skills="automating"/>
-                <SkillsBlock title="Interfaces" skills="interfaces"/>
-            </article>
 
-          </section>
-          <div className="home-contact">
-            <Button location="/contact">Contactez-moi</Button>
-          </div>
-
-        </main>
+          </main>
+        </div>
       </div>
     );
   }
