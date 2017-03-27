83ff67b5c16653aebc6b1b367ee5083b0a0f01bd
David Faby <david.faby29@gmail.com>
2017-03-27 06:14:00 +0000

    Implementation of routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 import './App.css';
 import david from './assets/img/david.jpg';
 import menu__icon from './assets/img/menu-icon.svg';
@@ -110,42 +114,10 @@ class SkillsBlock extends Component {
   }
 }
 
-class Portfolio extends Component {
+class HomeContent extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="parallax">
-          <div className="parallax__layer parallax__layer--front">
-            <header className="header">
-              <div className="logo">
-                <h2 className="logo__text">
-                  David <span className="logo__text--green">Faby</span>
-                </h2>
-              </div>
-              <nav className="menu">
-                <div className="menu__button">
-                  <a href="#" className="menu__button__link">
-                    <img className="menu__button__icon" src={menu__icon} alt="Menu" />
-                    <div className="menu__button__text">Menu</div>
-                  </a>
-                </div>
-                <ul className="menu__list">
-                  <li className="menu__list__item">Présentation</li>
-                  <li className="menu__list__item">Projets</li>
-                  <li className="menu__list__item">Contact</li>
-                </ul>
-              </nav>
-            </header>
-            <section className="intro">
-              <div className="intro__text">
-                <p className="intro__text__paragraph  intro__text__paragraph--primary">Bonjour !</p>
-                <p className="intro__text__paragraph"><span className="intro__text__paragraph--light">Je suis</span> David Faby,</p>
-                <h1 className="intro__title intro__title--close intro__title--code">Développeur<br/> Front-end</h1>
-              </div>
-              <div className="intro__pattern"></div>
-            </section>
-          </div>
-          <main className="content parallax__layer parallax__layer--back">
+      <main className="content parallax__layer parallax__layer--back">
             <section className="content-block">
               <img src={david} alt="David Faby" className="content-block__image image--parallax"/>
               <article className="article">
@@ -185,10 +157,101 @@ class Portfolio extends Component {
             </div>
 
           </main>
-        </div>
-      </div>
     );
   }
 }
 
+class ContactContent extends Component {
+  render() {
+    return (
+      <main className="content parallax__layer parallax__layer--back">
+            <section className="content-block">
+              <img src={david} alt="David Faby" className="content-block__image image--parallax"/>
+              <article className="article">
+                <h2 className="article__title">Présentation</h2>
+              </article>
+            </section>
+            <section className="content-block">
+              <article className="article article--color-invert article--space-bottom">
+                <h2 className="article__title">Compétences</h2>
+                  <SkillsBlock title="Intégration" skills="integration"/>
+                  <SkillsBlock title="Développement web" skills="development"/>
+                  <SkillsBlock title="Automatisation" skills="automating"/>
+                  <SkillsBlock title="Interfaces" skills="interfaces"/>
+              </article>
+
+            </section>
+            <div className="home-contact">
+              <Button location="/contact" type="primary">Contactez-moi !</Button>
+            </div>
+
+          </main>
+    );
+  }
+}
+
+class HomeIntro extends Component {
+  render() {
+    return (
+      <section className="intro">
+        <div className="intro__text">
+          <p className="intro__text__paragraph  intro__text__paragraph--primary">Bonjour !</p>
+          <p className="intro__text__paragraph"><span className="intro__text__paragraph--light">Je suis</span> David Faby,</p>
+          <h1 className="intro__title intro__title--close intro__title--code">Développeur<br/> Front-end</h1>
+        </div>
+        <div className="intro__pattern"></div>
+      </section>
+    );
+  }
+}
+
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <div className="parallax">
+            <div className="parallax__layer parallax__layer--front">
+              <header className="header">
+                <div className="logo">
+                  <h2 className="logo__text">
+                    David <span className="logo__text--green">Faby</span>
+                  </h2>
+                </div>
+                <nav className="menu">
+                  <div className="menu__button">
+                    <a href="#" className="menu__button__link">
+                      <img className="menu__button__icon" src={menu__icon} alt="Menu" />
+                      <div className="menu__button__text">Menu</div>
+                    </a>
+                  </div>
+                  <ul className="menu__list">
+                    <li className="menu__list__item">Présentation</li>
+                    <li className="menu__list__item">Projets</li>
+                    <li className="menu__list__item">Contact</li>
+                  </ul>
+                </nav>
+              </header>
+              
+              <Route exact path="/" component={HomeIntro} />
+              
+            </div>
+     
+     
+            <Route exact path="/" component={HomeContent} />
+            <Route path="/contact" component={ContactContent} />
+           
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+
+
 export default Portfolio;
+
+
+/*export default Portfolio;*/
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { browserHistory } from '../node_modules/react-router';
 import Portfolio from './App';
 import './index.css';
 
 
 ReactDOM.render(
-  <Portfolio />,
+  <Portfolio history={browserHistory} />,
   document.getElementById('root')
 );
+
