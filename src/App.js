da28295dbe89b27c97d7bcaf60b23dd4878c8307
David Faby <david.faby29@gmail.com>
2017-03-29 06:51:27 +0000

    Creation of Dialog component

--- src/App.js
+++ src/App.js
@@ -164,7 +164,21 @@ class Field extends Component {
   }
 }
 
-
+class Dialog extends Component {
+  render() {
+    return (
+      <div className={"dialog dialog--" + this.props.type}>
+        <div className="dialog__close">
+          <a href="#" className="dialog__close__link">
+            <i className="fa fa-times" aria-hidden="true"></i>
+          </a>
+        
+        </div>
+        <div className="dialog__message">{this.props.children}</div>
+      </div>
+    );
+  }
+}
 
 
 class Article extends Component {
