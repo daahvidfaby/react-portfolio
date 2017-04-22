3274429635dabb0c4630b4a69648a5fbf64d8b58
David Faby <contact@davidfaby.fr>
2017-04-23 01:32:32 +0200

    Exported 'about' part in external file, in case of translate

---
+++ src/content/about.js
@@ -0,0 +1,9 @@
+export default {
+    'title': 'Présentation',
+    'text': [
+         'Apprenti en <Strong>développement web</Strong>, j’aime réaliser des sites et des applications web interactives et modernes.',
+         'Plutôt orienté <Strong>Front-end</Strong>, je suis <Strong>polyvalent</Strong> lors de leur création. Pour vous, je saurais à la fois me pencher sur la conception, pour réfléchir au développement à toutes les étapes d’un projet.',
+         'Que ce soit sur la <Strong>réflexion</Strong> sur l’<Strong>expérience utilisateur</Strong>, l’<Strong>interface</Strong> et l’<Strong>accessibilité</Strong>, ou lors du <Strong>développement back-end</Strong> et front-end, je saurais utiliser mes compétences pour mener à bien ce projet.',       
+         'Je sais m’intégrer à une équipe et à la fois travailler en autonomie. Ensemble, nous pourrons créer l’avenir de l’internet.'
+    ]
+}
