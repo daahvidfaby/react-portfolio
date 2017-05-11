e246edbc15c3e950f07818b10faf4fae7c263632
David Faby <contact@davidfaby.fr>
2017-05-11 21:14:59 +0200

    Add of menu animation

--- src/App.js
+++ src/App.js
@@ -299,13 +299,19 @@ class Menu extends Component {
         </div>
         <ul className={"menu__list " + this.state.panelClassHandler}>
           <li className="menu__list__item">
-            <NavLink exact to="/" className="menu__list__link" activeClassName="active" onClick={this.handlePanel}>Présentation</NavLink>
+            <NavLink exact to="/" className="menu__list__link" activeClassName="active" onClick={this.handlePanel}>
+              <span className="menu__list__link__text">Présentation</span>
+             </NavLink>
           </li>
           <li className="menu__list__item">
-            <NavLink to="/projects" className="menu__list__link" activeClassName="active" onClick={this.handlePanel}>Projets</NavLink>
+            <NavLink to="/projects" className="menu__list__link" activeClassName="active" onClick={this.handlePanel}>
+              <span className="menu__list__link__text">Projets</span>
+             </NavLink>
           </li>
           <li className="menu__list__item">
-            <NavLink to="/contact" className="menu__list__link" activeClassName="active" onClick={this.handlePanel}>Contact</NavLink>
+            <NavLink to="/contact" className="menu__list__link" activeClassName="active" onClick={this.handlePanel}>
+              <span className="menu__list__link__text">Contact</span>
+            </NavLink>
           </li>
         </ul>
       </nav>
