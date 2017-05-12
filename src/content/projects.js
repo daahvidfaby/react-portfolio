3d0e4a11be8cfa80dded7f25b27cfc9a89ff9c3c
David Faby <david.faby29@gmail.com>
2017-05-12 05:16:17 +0000

    Projetcs text modification

--- src/content/projects.js
+++ src/content/projects.js
@@ -6,31 +6,31 @@ import paperPlane from '../assets/img/projects/paper-plane.jpg';
 
 export default [
   {
-    image: movieTracker,
-    title : 'Movie Tracker',
-    url : '/movie-tracker',
-    buttonText: 'Voir la démo',
-    description: "Lors d'un mini-projet de fin de module nous avons du réaliser par groupe de deux une web app imitant le comportement d'un site d'informations sur des films ou des séries, au choix. Il a fallu s'occuper du design et de tout le développement, le tout accédant aux informations au format JSON depuis l'API OMDB. J'ai utilisé le framework CSS : Materialize css. Le tout devait être en version mobile, je vous conseille donc de réduire la taille de l'écran."
-  },
-  {
     image: reactMail,
     title : 'React Gmail',
     url : 'https://react-mail.netlify.com/',
     buttonText: 'Voir la démo',
-    description: "Projet à réaliser obligatoirement avec le framework ReactJS. C'est un client gmail utilisant l'API de google en Javascript pour obtenir les différentes informations de la personne se connectant ainsi que ses e-mails. Aucune donnée n'est sauvegardée donc n'ayez pas peur de donner l'autorisation à accéder à votre compte pour la démo."
+    description: "Projet libre à réaliser obligatoirement avec le framework ReactJS. C'est un client gmail utilisant l'API de google en Javascript pour obtenir les différentes informations de l'utilisateur se connectant ainsi que ses e-mails. Aucune donnée n'est sauvegardée donc n'ayez pas peur de donner l'autorisation à accéder à votre compte pour la démo."
+  },
+  {
+    image: movieTracker,
+    title : 'Movie Tracker',
+    url : '/movie-tracker',
+    buttonText: 'Voir la démo',
+    description: "Lors d'un mini-projet de fin de module j'ai du réaliser une web app imitant le comportement d'un site d'informations sur des films ou des séries (type IMDB, Allociné, ...), au choix. Il a fallu s'occuper du design et de tout le développement. J'ai décidé de récupérer les informations au format JSON depuis la base de données OMDB, qui délivre une API. J'ai utilisé le framework CSS : Materialize css. Le tout devait être en version mobile, je vous conseille donc de réduire la taille de l'écran."
   },
   {
     image: reactWatch,
     title : 'React Watch',
     url : '/react-watch',
-    description: 'Premier projet réalisé avec ReactJS. Seules les aiguilles de la montre se mettent à jour grâce à la fonction setState(), principe de base du framework.',
+    description: 'Premier projet réalisé avec ReactJS. Seules les aiguilles de la montre se mettent à jour grâce à la fonction setState(), principe de base du framework. La montre et les aiguilles ont été crées en SVG, permettant un rendu optimal quelque soit la taille à afficher.',
     buttonText: 'Voir la démo'
   },
   {
     image: paperPlane,
     title : 'Paper Plane',
     url : '/paper-plane',
-    description: 'Lorem ipsum lqskdjqsljd qslkd qslkdj qslkj dqslkj qdskl',
+    description: 'Paper Plane est un jeu développé en HTML5 et Javascript. Réalisé à l\'aide de la librairie PhaserJS, on y retrouve un personnage qui avance le long d\'un décor généré aléatoirement. Il faut avancer le plus loin possible, tout en récupérant des bonus pour augmenter le score.',
     buttonText: 'Voir la démo'
   },
 ];
